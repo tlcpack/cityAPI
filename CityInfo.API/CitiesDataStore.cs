8d15cefe95184e37eeafe77a3884bc5a8340f7b7
Taylor Cooke <tcooke@relias.com>
2021-02-15 13:55:40 -0500

    retrieves children

--- CityInfo.API/CitiesDataStore.cs
+++ CityInfo.API/CitiesDataStore.cs
@@ -22,8 +22,8 @@ namespace CityInfo.API
                      Id = 1,
                      Name = "New York City",
                      Description = "The one with that big park.",
-                     // PointsOfInterest = new List<PointOfInterestDto>()
-                     /* {
+                     PointsOfInterest = new List<PointOfInterestDto>()
+                      {
                          new PointOfInterestDto() {
                              Id = 1,
                              Name = "Central Park",
@@ -32,14 +32,14 @@ namespace CityInfo.API
                              Id = 2,
                              Name = "Empire State Building",
                              Description = "A 102-story skyscraper located in Midtown Manhattan." },
-                     } */
+                     } 
                 },
                 new CityDto()
                 {
                     Id = 2,
                     Name = "Antwerp",
                     Description = "The one with the cathedral that was never really finished.",
-                    /* PointsOfInterest = new List<PointOfInterestDto>()
+                    PointsOfInterest = new List<PointOfInterestDto>()
                      {
                          new PointOfInterestDto() {
                              Id = 3,
@@ -49,14 +49,14 @@ namespace CityInfo.API
                              Id = 4,
                              Name = "Antwerp Central Station",
                              Description = "The the finest example of railway architecture in Belgium." },
-                     } */
+                     } 
                 },
                 new CityDto()
                 {
                     Id= 3,
                     Name = "Paris",
                     Description = "The one with that big tower.",
-                    /* PointsOfInterest = new List<PointOfInterestDto>()
+                    PointsOfInterest = new List<PointOfInterestDto>()
                      {
                          new PointOfInterestDto() {
                              Id = 5,
@@ -66,7 +66,7 @@ namespace CityInfo.API
                              Id = 6,
                              Name = "The Louvre",
                              Description = "The world's largest museum." },
-                     } */
+                     }
                 }
             };
         }
