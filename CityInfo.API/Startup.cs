6717bcbfbaffcd0ba47e054acc083b81723f2568
Taylor Cooke <tcooke@relias.com>
2021-02-15 14:03:24 -0500

    module complete. some mvc settings added

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +17,19 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .AddMvcOptions(o =>
+                {
+                    o.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+                });
+            /*    .AddJsonOptions(o =>
+            {
+                if (o.SerializerSettings.ContractResolver != null)
+                {
+                    var casterResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                    casterResolver.NamingStrategy = null;
+                }
+            });*/
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
