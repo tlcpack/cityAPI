d2d9a5603b355aa5337c8b0b9c4392c8546748ba
Taylor Cooke <tcooke@relias.com>
2021-02-15 13:29:01 -0500

    module 3 complete, api returning data and codes

--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -15,6 +15,7 @@ namespace CityInfo.API
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -29,11 +30,10 @@ namespace CityInfo.API
                 app.UseExceptionHandler();
             }
 
-            app.Run((context) =>
-            {
-                // await context.Response.WriteAsync("Hello World!");
-                throw new Exception("Example exception");
-            });
+            app.UseStatusCodePages();
+
+            app.UseMvc();
+
         }
     }
 }
